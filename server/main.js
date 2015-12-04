8be26261a0eb8829b8d7e6d6fd4d154708c9daf5
Patrick Feehery Kirts <patkirts@gmail.com>
2015-12-04 13:22:02 -0700

    babel does flowtype checking now

--- server/main.js
+++ server/main.js
@@ -1,2 +1,10 @@
 import settings from '../common/settings';
 console.log("\n*** Starting Main Node Program ***\nsettings:", settings);
+
+type Foo = string|number;
+function foo(bar: Foo): Foo {
+    console.log(`foo: ${bar}`);
+    return bar;
+}
+
+foo(42);
