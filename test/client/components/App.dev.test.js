59b87461e8bac762e6be9f630eb935e06de36273
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-02 22:28:42 -0700

    fixed propType warnings in App.dev test

--- test/client/components/App.dev.test.js
+++ test/client/components/App.dev.test.js
@@ -4,12 +4,13 @@ import { createRenderer } from 'react-addons-test-utils';
 //import render from 'react-shallow-renderer';
 
 import App from '../../../client/components/App/App.dev';
+// @TODO this should be a spy
 import DevTools from '../../../client/components/App/DevTools';
 
 describe('COMPONENT: App.dev', () => {
   it('woo', () => {
     const renderer = createRenderer();
-    renderer.render(<App store={{}} />);
+    renderer.render(<App dispatch={() => {}} count={0} />);
     const app = renderer.getRenderOutput();
     expect(app.props.children[1].type).to.equal(DevTools);
   });
