9944b0d91c5b53c4ca9c354a66bba8cae89c4695
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-03 00:46:21 -0700

    login submit event handled at component-level

--- client/components/user/auth/Login.js
+++ client/components/user/auth/Login.js
@@ -1,11 +1,42 @@
-import React, { Component, PropTypes } from 'react';
+import { expect } from 'chai';
+import React, { PropTypes } from 'react';
+import getFormData from 'get-form-data';
 
 import { login } from '../../../actions/user';
+import { PROD } from '../../../../common/constants/env';
+
+/**
+ * Handles the onSubmit event for the form,
+ * validates the form data,
+ * and returns the callback with the correct action.
+ *
+ * @param  {SyntheticEvent} e - React form event
+ * @param  {function} submitCallback - callback taking action object
+ * @returns {function} calls callback
+ */
+function handleSubmit(e, submitCallback) {
+  if (process.env.NODE_ENV !== PROD) {
+    expect(submitCallback).to.be.a('function');
+  }
+
+  e.preventDefault();
+
+  const formData = getFormData(e.currentTarget);
+  console.log("form data", formData);
+
+  // @TODO validation
+  // just "required" in the HTML right now
+
+  // this will dispatch the action
+  return submitCallback(login(formData));
+}
 
 const Login = props => (
-  <form onSubmit = {() => props.onSubmit(login())}>
-    <input type="text" name="username" placeholder="username" />
-    <input type="password" name="password" placeholder="pass phrase" />
+  <form onSubmit={(e) => handleSubmit(e, props.onSubmit)}>
+    <input type="text" name="username" placeholder="username"
+      required />
+    <input type="password" name="password" placeholder="pass phrase"
+      required />
     <button type="submit">Login</button>
   </form>
 );
