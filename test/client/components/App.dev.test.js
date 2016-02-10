816ab3693c0384bfc2d85f9fa43ecad03959cf95
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-09 23:56:37 -0700

    App.dev has tests with all child components stubbed

--- test/client/components/App.dev.test.js
+++ test/client/components/App.dev.test.js
@@ -8,74 +8,83 @@ import { createRenderer } from 'react-addons-test-utils';
 
 chai.use(sinonChai);
 
-// child component stubs
-// class DevTools extends Component {
-//   render() {
-//     return <div />;
-//   }
-// }
-
-// const DevTools = React.createClass({
-//   render() {
-//     return <div />;
-//   }
-// });
-
-const DevTools = () => (<div>
- hello world
-</div>);
-
-// import App from '../../../client/components/App/App.dev';
+// Child Component stubs
+const Counter = () => <div />;
+const DevTools = () => <div />;
+const Login = () => <div />;
+const Logout = () => <div />;
 
 const App = proxyquire('../../../client/components/App/App.dev', {
+  '../Counter': {
+    __esModule: true,
+    '@noCallThru': true,
+    default: Counter
+  },
   './DevTools': {
     __esModule: true,
     '@noCallThru': true,
     default: DevTools
+  },
+  '../user/auth/Login': {
+    __esModule: true,
+    '@noCallThru': true,
+    default: Login
+  },
+  '../user/auth/Logout': {
+    __esModule: true,
+    '@noCallThru': true,
+    default: Logout
   }
 }).default;
 
-describe('COMPONENT: App.dev', () => {
+describe('COMPONENT: App.dev without user', () => {
   const renderer = createRenderer();
+
+  renderer.render(<App dispatch={() => {}} count={0}
+    userRecord={{}} />);
+
+  const appWithoutUser = renderer.getRenderOutput();
+  const children = appWithoutUser.props.children;
+
+  it('should render Login as first child', () => {
+    expect(children[0].type).to.equal(Login);
+  });
+
+  it('should render Counter second to last', () => {
+    expect(children[children.length-2].type)
+      .to.equal(Counter);
+  });
 
   /* @TODO really spec this shit out */
   it('should have a last child of type DevTools', () => {
-    renderer.render(<App dispatch={() => {}} count={0}
-      userRecord={{ user: { username: 'pat' } }} />);
-
-    const app = renderer.getRenderOutput();
-
-    expect(app.props.children[app.props.children.length-1].type)
+    expect(children[children.length-1].type)
       .to.equal(DevTools);
   });
 
-  // Start by writing a bunch of should statements
+});
 
-  it('should render Login if passed a user', () => {
+describe('COMPONENT: App.dev with user', () => {
+  const renderer = createRenderer();
 
+  renderer.render(<App dispatch={() => {}} count={0}
+    userRecord={{ user: {} }} />);
+
+  const appWithUser = renderer.getRenderOutput();
+  const children = appWithUser.props.children;
+
+  it('should  render Logout', () => {
+    expect(children[0].type).to.equal(Logout);
   });
 
-  it('should ', () => {
-
+  it('should render Counter second to last', () => {
+    expect(children[children.length-2].type)
+      .to.equal(Counter);
   });
 
-  it('should ', () => {
-
+  /* @TODO really spec this shit out */
+  it('should have a last child of type DevTools', () => {
+    expect(children[children.length-1].type)
+      .to.equal(DevTools);
   });
 
-  it('should ', () => {
-
-  });
-
-  it('should ', () => {
-
-  });
-
-  it('should ', () => {
-
-  });
-
-  it('should ', () => {
-
-  });
 });
