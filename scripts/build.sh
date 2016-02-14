7acbd11a7d356b818c07e72a470abde884a7742a
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-13 23:37:40 -0700

    removed sourcemaps from client js

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ mkdir build/public
 
 vulcanize --exclude /*.js client/index.prod.html > build/public/index.html
 
-browserify -d client/index.js -o build/index.js
+browserify client/index.js -o build/public/index.js
 
 cp public/socketcluster.min.js build/public/
 cp package.json .babelrc build/
