e7b1ea78831d49d07d3dffb99adb15cf2695114d
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-14 00:24:15 -0700

    uglification

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ mkdir build/public
 vulcanize --exclude /*.js client/index.prod.html > build/public/index.html
 
 browserify client/index.js -o build/public/index.js
+uglifyjs -c -m -o build/public/index.js -- build/public/index.js
 
 cp public/socketcluster.min.js build/public/
 cp package.json .babelrc build/
