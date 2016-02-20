edd6c427ad851a1246b61f9bb54886279b9a4846
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-20 05:41:16 -0700

    moved that util in test/ for cleanliness

---
+++ test/utils/expectStateCollection.dev.test.js
@@ -0,0 +1,17 @@
+import { expect, AssertionError } from 'chai';
+import expectStateCollection from '../../src/utils/expectStateCollection.dev';
+
+describe('TEST_UTIL: expectStateCollection', () => {
+  it('should return `true` if supplied { ids[], dict{} }', () => {
+    const param = { ids: [], dict: {} };
+    expect(expectStateCollection(param)).to.be.true;
+  });
+
+  it('should throw AssertionError otherwise', () => {
+    expect(() => expectStateCollection()).to.throw(AssertionError);
+    expect(() => expectStateCollection({ ids: [] })).to.throw(AssertionError);
+    expect(() => expectStateCollection({ ids: [], dict: 'foo '})).to.throw(AssertionError);
+    expect(() => expectStateCollection(42)).to.throw(AssertionError);
+    expect(() => expectStateCollection(true)).to.throw(AssertionError);
+  });
+})
