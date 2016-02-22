a2ea8535fc77b2cbb7cb44c8e5c8354e1a14683a
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-22 01:24:23 -0700

    test file eextension was missing

--- src/server/worker.js
+++ src/server/worker.js
@@ -9,7 +9,8 @@ import socket from './socket';
  */
 export function run(worker) {
   console.log(`   >> SC Worker PID: ${process.pid}`);
-  
+
+  // NB call `app`, but pass in `socket` to be called.
   worker.httpServer.on('request', app());
   worker.scServer.on('connection', socket);
 }
