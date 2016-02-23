358348a68dec3a6ba8d62949c1bb31b0e0e35fb7
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-22 18:46:26 -0700

    comments and done with that file

--- src/expectations/expectString.js
+++ src/expectations/expectString.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 
 /**
- * [expectString description]
- * @param  {[type]} string [description]
- * @return {[type]}        [description]
+ * Expect a string, return true if passes.
+ * @param  {[type]} string - value to expect
+ * @return {boolean} `string` is a 'string'
  */
 export default function expectString(string) {
   expect(string).to.be.a('string');
