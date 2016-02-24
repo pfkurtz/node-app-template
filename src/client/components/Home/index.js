03f051ddffe2eb580911fd0b1f351eecd053cce4
Patrick Feehery Kirts <patkirts@gmail.com>
2016-02-24 02:25:45 -0700

    client modularity 2

--- src/client/components/Home/index.js
+++ src/client/components/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = props => (
   <div>
-    <h1>This is the home page</h1>
+    <h1>This is the home page {props.foo}</h1>
   </div>
 );
 
--- src/client/ui/Root.js
+++ src/client/ui/Root.js
@@ -1,38 +1,13 @@
 import React, { PropTypes } from 'react';
-import { connect, Provider } from 'react-redux';
-import {
-  Router,
-  Route,
-  IndexRoute,
-  browserHistory
-} from 'react-router';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from '../components';
-import Home from '../components/Home';
-import NotFound from '../components/errors/NotFound';
-
-/**
- * `props` for `RootComponent` (`App`)
- * @param  {Object} state - from first call of store reducer
- * @return {Object} `props`
- */
-function appProps(state) {
-  return {
-    count: state.count,
-    location: state.routing.location,
-    userRecord: state.userRecord
-  };
-}
-
-const RootComponent = connect(appProps)(App);
+import HomeRoute from '../routes/HomeRoute';
 
 const Root = props => (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={RootComponent}>
-        <IndexRoute component={Home} />
-        <Route path="*" component={NotFound} />
-      </Route>
+      <HomeRoute />
     </Router>
   </Provider>
 );
@@ -42,3 +17,4 @@ Root.propTypes = {
 };
 
 export default Root;
+hhhgv
